31da1ef3cfded19c71a2b2cac008370fb5c82ce1
Paradox Programming <41707197+theParadox42@users.noreply.github.com>
2019-05-13 22:06:25 -0600

    Update preload.js

--- scripts/preload.js
+++ scripts/preload.js
@@ -1,5 +1,6 @@
 var imgs = {};
 
 function preload(){
-    imgs.player = loadImage("/art/player.png")
+    imgs.player = loadImage("/art/player.png");
+    imgs.cloud = loadGif("/art/clouds.gif");
 }
