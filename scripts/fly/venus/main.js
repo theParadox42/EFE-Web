736ede381613ac19a86f8501df404b829903c31d
Tannerderp <tannersingleton9@gmail.com>
2019-05-25 17:59:49 -0600

    fly to venus level
    can someone make a boss fight ufo sprite?

--- scripts/fly/venus/main.js
+++ scripts/fly/venus/main.js
@@ -1,3 +1,4 @@
+height = window.innerHeight;
 function flyToVenus(){
     background(0,0,0);
     push();
@@ -11,18 +12,31 @@ function flyToVenus(){
           lasers.splice(i, 1);
       }
     }
+    for(var i = ufos.length-1; i>-1; i--){
+        ufos[i].run(flyPlayer);
+        if(ufos[i].dead&&ufos[i].frame>20){
+            ufos.splice(i, 1);
+        }
+    }
     flyPlayer.run();
-    for(var i in asteroids){
+    for(var i  = asteroids.length-1; i>-1; i--){
         asteroids[i].run(flyPlayer);
         if(asteroids[i].dead&&asteroids[i].frame>20){
             asteroids.splice(i, 1);
         }
     }
     pop();
+    flyPlayer.displayHealth();
+    if(flyPlayer.x>4000){
+        game.continue();
+    }
+}
+flyToVenus.level = {
+    asteroids: [[1200, height/2, height/2], [3000, height/2, 100], [3000, height/2-200, 100], [3000, height-200, 100]],
+    ufos: [[1600, height/2], [2000, 100], [2400, height-100], [2800, height/2], [3500, height/2], [3500, height]],
 }
 flyToVenus.init = function(){
     flyPlayer = new FlyPlayer(50, height/2);
     flyPlayer.init();
-    flySigns = [];
-    asteroids = [];
+    loadLevel(this.level);
 }
