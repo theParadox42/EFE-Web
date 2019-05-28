a20eb816f1f6eb3ff315e17eb7a4ec87d5924a94
Paradox Programming <nathaniel.fargo@gmail.com>
2019-05-28 15:31:38 +0000

    switched scene order

--- scripts/game.js
+++ scripts/game.js
@@ -6,7 +6,7 @@ var game = {
     // Slash out all the scenes until the one you are working on EXCEPT for "load"
     sceneOrder: [
         "load",
-        "communitylevels",
+        // "communitylevels",
         "home",
         "run",
         "build",
@@ -15,7 +15,7 @@ var game = {
         "fly-mars",
         "fight",
         "fly-venus",
-        "ufo",
+        // "ufo",
         "won"
         // Other scenes will probably include, "levelbuilder", "leveltester"
     ],
